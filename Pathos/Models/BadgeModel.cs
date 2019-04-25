935ddd11b784de904ca3aec0d84f1f8671a3e783
Derek Drummond <ddrummond@cylance.com>
2019-04-25 09:57:56 -0500

    Add Badge Model

---
+++ Pathos/Models/BadgeModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pathos.Models
+{
+    [Table("badges")]
+    public class Badge
+    {
+        public int BadgeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
